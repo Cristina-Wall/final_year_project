fe9ce266441cfd56b725a7872e4ea92ba53ec0ec
Cristina-Wall <c.wall16@universityofgalway.ie>
2024-03-09 17:15:22 +0000

    Added functions to make scales

--- markov_chain.py
+++ markov_chain.py
@@ -68,8 +68,128 @@ def get_file_names(directory):
             file_names.append(directory + "/" + filename)
     return file_names
 
-# directory_path = "midiFiles/maestro-v3.0.0/2004"
-directory_path = "midiFiles/one_note_melodies"
+def make_scale_of_c():
+    scale = []
+    for x in range(128):
+        if x % 12 == 0:
+            scale.append(x)
+        elif x % 12 == 2:
+            scale.append(x)
+        elif x % 12 == 4:
+            scale.append(x)
+        elif x % 12 == 5:
+            scale.append(x)
+        elif x % 12 == 7:
+            scale.append(x)
+        elif x % 12 == 9:
+            scale.append(x)
+        elif x % 12 == 11:
+            scale.append(x)
+    return scale
+
+def get_allowed_notes(key, tonality):
+    scale_of_major_c = make_scale_of_major_c()
+    scale_of_minor_c = make_scale_of_minor_c()
+
+    if key == "c" and tonality == "major":
+        return scale_of_major_c
+    elif key == "c" and tonality == "minor":
+        return scale_of_minor_c
+    elif tonality == "major":
+        scale = make_scale(key, scale_of_major_c)
+        return scale
+    elif tonality == "minor":
+        scale = make_scale(key, scale_of_minor_c)
+        return scale
+    else:
+        return 0
+
+def make_scale_of_major_c():
+    scale = []
+    for x in range(128):
+        if x % 12 == 0:
+            scale.append(x)
+        elif x % 12 == 2:
+            scale.append(x)
+        elif x % 12 == 4:
+            scale.append(x)
+        elif x % 12 == 5:
+            scale.append(x)
+        elif x % 12 == 7:
+            scale.append(x)
+        elif x % 12 == 9:
+            scale.append(x)
+        elif x % 12 == 11:
+            scale.append(x)
+    return scale
+
+def make_scale_of_minor_c():
+    scale = []
+    for x in range(128):
+        if x % 12 == 0:
+            scale.append(x)
+        elif x % 12 == 2:
+            scale.append(x)
+        elif x % 12 == 3:
+            scale.append(x)
+        elif x % 12 == 5:
+            scale.append(x)
+        elif x % 12 == 7:
+            scale.append(x)
+        elif x % 12 == 8:
+            scale.append(x)
+        elif x % 12 == 11:
+            scale.append(x)
+    return scale
+
+def make_scale(key, scale_of_c):
+    scale = len(scale_of_c)*[0]
+    if key == "d":
+        for i in range(len(scale_of_c)):
+            scale[i] = scale_of_c[i] + 2
+    elif key == "e":
+        for i in range(len(scale_of_c)):
+            scale[i] = scale_of_c[i] + 4
+    elif key == "f":
+        for i in range(len(scale_of_c)):
+            scale[i] = scale_of_c[i] + 5
+    elif key == "g":
+        for i in range(len(scale_of_c)):
+            scale[i] = scale_of_c[i] + 7
+    elif key == "a":
+        for i in range(len(scale_of_c)):
+            scale[i] = scale_of_c[i] + 9
+    elif key == "b":
+        for i in range(len(scale_of_c)):
+            scale[i] = scale_of_c[i] + 11
+    elif key == "d_flat" or key == "c_sharp":
+        for i in range(len(scale_of_c)):
+            scale[i] = scale_of_c[i] + 1
+    elif key == "e_flat" or key == "d_sharp":
+        for i in range(len(scale_of_c)):
+            scale[i] = scale_of_c[i] + 3
+    elif key == "g_flat" or key == "f_sharp":
+        for i in range(len(scale_of_c)):
+            scale[i] = scale_of_c[i] + 6
+    elif key == "a_flat" or key == "g_sharp":
+        for i in range(len(scale_of_c)):
+            scale[i] = scale_of_c[i] + 8
+    elif key == "b_flat" or key == "a_sharp":
+        for i in range(len(scale_of_c)):
+            scale[i] = scale_of_c[i] + 10
+    else:
+        return "invalid key"
+    
+    removed = 0
+
+    for i in range(len(scale) - removed):
+        if scale[i - removed] > 128:
+            scale.remove(scale[i - removed])
+            removed += 1
+    return scale
+
+directory_path = "midiFiles/maestro-v3.0.0/2004"
+# directory_path = "midiFiles/one_note_melodies"
 file_names = get_file_names(directory_path)
 
 all_tunes = []
@@ -129,9 +249,13 @@ time = 0  # current time in tune
 curr_note = 59  # starting note
 length = 500  # length of song in notes
 next_note_temp = 0
+chosen_key = "c"
+chosen_tonality = "major"
 
 channel = 0
 pitch = 0
+
+notes_allowed = get_allowed_notes(chosen_key, chosen_tonality)
 
 track.append(mido.MetaMessage('set_tempo', tempo=120, time=0))
 
@@ -140,13 +264,17 @@ for i in range(500):
     track.append(mido.Message('note_on', note=curr_note, velocity=velocity, time=time))
     track.append(mido.Message('note_off', note=curr_note, velocity=velocity, time=time+duration))
     time += duration
-    
-    # midi_file.addNote(channel, i, pitch+i, duration, 100)
-
     # Generate the next note based on the probabilities
-    next_note_temp = select_next_note(normalised_probabilities, curr_note)
+    next_note_temp = select_next_note(normalised_probabilities, curr_note, notes_allowed)
     curr_note = next_note_temp
 
+# print(normalised_probabilities)
 # Save the MIDI file
-# midi_file.save("output/output_markov28.mid")
+# midi_file.save("output/output_markov_dataset04_01.mid")
 # print(midi_file)
+
+# writing to file
+# file1 = open('output_09_03_dataset04_probs.txt', 'w')
+# for i in range(len(normalised_probabilities)):
+#     file1.write(str(normalised_probabilities[i]) + "\n")
+# file1.close()
