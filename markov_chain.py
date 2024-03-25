d450a19319227f30df7b850465a1e1822f122e72
Cristina-Wall <c.wall16@universityofgalway.ie>
2024-03-25 20:38:49 +0000

    Small fixes

--- markov_chain.py
+++ markov_chain.py
@@ -1,3 +1,5 @@
+# website connecting branch
+
 from random import randint
 import copy
 import datetime
@@ -8,6 +10,7 @@ import os
 import ast
 import math
 
+# Function that will get the next note based on the probabilities, if the program is running based on a single note
 def select_next_note(input_notes, curr_note, notes_allowed):
     while True:
         probability_distribution = copy.copy(input_notes[curr_note])
@@ -19,7 +22,36 @@ def select_next_note(input_notes, curr_note, notes_allowed):
             if random_value <= cumulative_prob and index in notes_allowed:
                 return index
 
+# Similar to function above but this uses the probabilities based on the two previous notes
+def select_next_note_from_two(input_notes, curr_note, prev_note, notes_allowed, num_notes):
+    while True:
+        probability_distribution = copy.copy(input_notes[(prev_note * num_notes) + curr_note])
+        cumulative_probabilities = [sum(probability_distribution[:idx+1]) for idx in range(len(probability_distribution))]
+        random_value = random.uniform(0, 1)
 
+        # Find the index where the random_value falls in the cumulative probabilities
+        for index, cumulative_prob in enumerate(cumulative_probabilities):
+            if random_value <= cumulative_prob:
+                if index in notes_allowed:
+                    # Return the note found
+                    return index
+
+# Similar to function above but this uses the probabilities based on the three previous notes
+def select_next_note_from_three(input_notes, curr_note, prev_note, prev_prev_note, notes_allowed, num_notes):
+    while True:
+        probability_distribution = copy.copy(input_notes[((prev_prev_note * num_notes)^2) + (prev_note * num_notes) + curr_note])
+        cumulative_probabilities = [sum(probability_distribution[:idx+1]) for idx in range(len(probability_distribution))]
+        random_value = random.uniform(0, 1)
+
+        # Find the index where the random_value falls in the cumulative probabilities
+        for index, cumulative_prob in enumerate(cumulative_probabilities):
+            if random_value <= cumulative_prob:
+                if index in notes_allowed:
+                    # Return the note found
+                    return index
+
+# Function that imports a midi file and removes any duplicate tracks
+# this is not used
 def import_file(file_name):
     mid = MidiFile(file_name, clip=True)
     message_numbers = []
@@ -37,6 +69,8 @@ def import_file(file_name):
     return mid
 
 
+# Function that goes through all the files in the directory and gets the note values from each file
+# Each note gets added to a list in the order that they occur so that the probabilities can be calculated later
 def get_notes_from_file(file_name):
     mid = MidiFile(file_name, clip=True)
     mididict = []
@@ -62,6 +96,7 @@ def get_notes_from_file(file_name):
     output = [item for sublist in output for item in sublist]
     return output, total_notes
 
+# Function that gets all the file names from the input directory
 def get_file_names(directory):
     file_names = []
     for dir in directory:
@@ -70,7 +105,10 @@ def get_file_names(directory):
                 file_names.append(dir + "/" + filename)
     return file_names
 
+# Function that gets the allowed notes based on the key and tonality
+# The program will later then only be allowed to select notes from this list
 def get_allowed_notes(key, tonality):
+
     if key == "c" and tonality == "major":
         scale_of_major_c = make_scale_of_major_c()
         return scale_of_major_c
@@ -88,6 +126,8 @@ def get_allowed_notes(key, tonality):
     else:
         return 0
 
+# Function that makes a scale of C major, spanning across 0 to 128 notes (the range in a midi file)
+# All major scales are based on the C scale, adding the number of semitones to each note in the scale
 def make_scale_of_major_c():
     scale = []
     # The scale of C is made up of notes number 0, 2, 4, 5, 7, 9, 11 in every octave
@@ -108,6 +148,8 @@ def make_scale_of_major_c():
             scale.append(x)
     return scale
 
+# Function that makes a scale of C minor, spanning across 0 to 128 notes (the range in a midi file)
+# All minor scales are based on the C scale, adding the number of semitones to each note in the scale
 def make_scale_of_minor_c():
     scale = []
     # The scale of C is made up of notes number 0, 2, 3, 5, 7, 8, 11 in every octave
@@ -128,6 +170,7 @@ def make_scale_of_minor_c():
             scale.append(x)
     return scale
 
+# Function that makes a scale based on the key and the scale of C, adding the number of semitones to each note in the scale
 def make_scale(key, scale_of_c):
     scale = len(scale_of_c)*[0]
     if key == "d":
@@ -175,6 +218,7 @@ def make_scale(key, scale_of_c):
             removed += 1
     return scale
 
+# Function that creates the initial empty arrays
 def start_probs(num_notes, num_past_notes):
     probabilities_array = [[0] * num_notes for _ in range(pow(num_notes, num_past_notes))]
     normalised_probabilities = [[0] * num_notes for _ in range(pow(num_notes, num_past_notes))]
@@ -182,6 +226,7 @@ def start_probs(num_notes, num_past_notes):
     states = list(range(pow(num_notes, num_past_notes)))
     return probabilities_array, normalised_probabilities, probabilities_note_count, states
 
+# Function that counts the occurrences of each note in the tunes
 def count_notes_for_probs(states, num_notes, probabilities_array, all_tunes, probabilities_note_count, num_past_notes):
     for i in range(len(states)):
         for j in range(num_notes):
@@ -384,6 +429,10 @@ def make_song(instrument_chosen, tonality_chosen, tempo_chosen, key_chosen):
 
         # Generate the next note based on the probabilities
         next_note_temp = select_next_note(normalised_probabilities, curr_note, notes_allowed)
+        # next_note_temp = select_next_note_from_two(normalised_probabilities, curr_note, prev_note, notes_allowed, num_notes)
+        # next_note_temp = select_next_note_from_three(normalised_probabilities, curr_note, prev_note, prev_prev_note, notes_allowed, num_notes)
+
+        prev_prev_note = prev_note
         prev_note = curr_note
         curr_note = next_note_temp
 
